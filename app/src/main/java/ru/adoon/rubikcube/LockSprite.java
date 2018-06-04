b501e2b9a679e65619375fc13b5cab2b8e2bdf6f
Sergey <lukshin.sa@yandex.ru>
2018-06-04 13:09:07 +0300

    1

---
+++ app/src/main/java/ru/adoon/rubikcube/LockSprite.java
@@ -0,0 +1,8 @@
+package ru.adoon.rubikcube;
+
+/**
+ * Created by Лукшин on 28.05.2018.
+ */
+
+public class LockSprite {
+}
---
+++ app/src/main/java/ru/adoon/rubikcube/TimeInfo.java
@@ -0,0 +1,8 @@
+package ru.adoon.rubikcube;
+
+/**
+ * Created by Лукшин on 29.05.2018.
+ */
+
+public class TimeInfo {
+}
